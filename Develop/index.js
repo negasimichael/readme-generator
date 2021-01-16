4564f824dba5372e4d3378765c187a1cf8f42771
negasimichael <negasimichael>
2021-01-16 00:21:41 -0600

    updated details

--- Develop/index.js
+++ Develop/index.js
@@ -10,42 +10,50 @@ const questions = [
      name: 'title',
      message: 'What is the title of the project? (Required)',
     },
+
     //Description
     {type: 'input',
      name: 'description',
      message: 'Provide a breif description of the project. (Required)',
     },
+
     //Installation information
     {type: 'input',
      name: 'installation',
      message: 'What are the installation requierements and guide for this project? (Required)',
     },
+
     //Usage Instructions
     {type: 'input',
      name: 'usage',
      message: 'Provide brief directions for use and include gifs and/or screenshots as needed. (Required)',
     },
+
     // License and Badge
     {type: 'list',
      name: 'license',
      message: 'Select a license for this project. (Required)',
      choices: ['MIT','ISC', 'Zlib', 'None']
     },
+
     //contribution
     {type: 'input',
      name: 'contribution',
      message: 'Provide guidelines how to contribute to this Project.  (Required)',
     },
+
     //linkden
     {type: 'input',
      name: 'linkdin',
      message: 'What is LinkdIn profile.  (Optional)',
     },
+
     //GitHub
     {type: 'input',
      name: 'url',
      message: 'What is your deployed URL?',
     },
+
     //Email
     {type: 'input',
      name: 'email',
